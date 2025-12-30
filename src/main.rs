20657816c2b41241dd7693e2221187d19498da53
Arne Beer <contact@arne.beer>
2025-12-30 08:19:22 +0100

    feat: Multithreading and volume adjustment

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use std::env;
+use std::fs::File;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::path::PathBuf;
+use std::time::Duration;
 
 use hound::WavReader;
+use serde::Deserialize;
 
-fn client_func(
-    wav_path: &str,
-    server_ip: &str,
-    port: u16,
-    second_offset: usize,
-) -> anyhow::Result<()> {
-    let mut reader = WavReader::open(wav_path)?;
+fn client_func(file: WavFile, server_address: String, port: u16) -> anyhow::Result<()> {
+    let mut reader = WavReader::open(&file.path)?;
     let spec = reader.spec();
     let fps = spec.sample_rate as usize;
     let total_frames = reader.duration() as usize;
 
-    let frame_index = second_offset * fps;
+    let frame_index = file.start_offset * fps;
     if frame_index >= total_frames {
         anyhow::bail!("Offset is beyond total frames");
     }
@@ -44,7 +42,7 @@ fn client_func(
         samples
     };
 
-    let mut stream = TcpStream::connect((server_ip, port))?;
+    let mut stream = TcpStream::connect((server_address, port))?;
     stream.write_all(b"CONFIG\n")?;
     stream.set_read_timeout(Some(std::time::Duration::from_secs(2)))?;
 
@@ -55,32 +53,57 @@ fn client_func(
 
     let mut counter = 1;
     loop {
-        println!("Sending sample: {counter}");
+        println!("Sending sample for {:?}: {counter}", &file.path);
         for (position, sample) in samples.iter().enumerate() {
             // Only send as many packets as we're allowed to
             if position >= max_size {
                 break;
             }
 
-            let value = *sample as f32 / 32768.0;
+            let value = *sample as f32 / 32768.0 * file.volume_adjustment;
             let message = format!("SMPL {} {:.7}\n", position, value);
             stream.write_all(message.as_bytes())?;
         }
+        std::thread::sleep(Duration::from_millis(1000));
         counter += 1;
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        anyhow::bail!("Usage: {} <wav_file> [second_offset]", args[0]);
+    let config: Config = serde_yaml::from_reader(File::open("config.yml")?)?;
+
+    for file in config.files {
+        let server = config.server.clone();
+        let port = config.port;
+        let file_clone = file.clone();
+
+        std::thread::spawn(move || {
+            let error = format!("Failed for {:?}", &file_clone.path);
+            client_func(file_clone, server, port).expect(&error);
+        });
     }
 
-    let wav_file = &args[1];
-    let second_offset = args.get(2).and_then(|s| s.parse().ok()).unwrap_or(0);
-    let server = "sampleflut.de";
-    let port = 8080;
-
-    client_func(wav_file, server, port, second_offset)?;
+    std::thread::sleep(Duration::MAX);
     Ok(())
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Config {
+    files: Vec<WavFile>,
+    server: String,
+    port: u16,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct WavFile {
+    path: PathBuf,
+    #[serde(default)]
+    start_offset: usize,
+    // Percentage volume assignment
+    #[serde(default = "default_adjustment")]
+    volume_adjustment: f32,
+}
+
+fn default_adjustment() -> f32 {
+    1.0
 }
